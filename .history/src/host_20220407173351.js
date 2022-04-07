623a952749c9c845b3d9d018c8a45ff12b2021e9
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2022-04-07 17:38:09 +0500

    yttj

---
+++ .history/src/host_20220407173351.js
@@ -0,0 +1,9 @@
+export const url="https://buxoro.vatanparvar.uz/Files/News"
+export const urlV="https://vatanparvar.uz/Files/News"
+export const api="https://buxoro.vatanparvar.uz/"
+export const urlFoto="https://buxoro.vatanparvar.uz/Files/Photogallery"
+export const urlStat="https://buxoro.vatanparvar.uz/Files/StatisticElements"
+export const urlPer="https://buxoro.vatanparvar.uz/Files/Personal"
+export const region=14
+export const regionI=1706
+export const urlImg="https://kadr.vatanparvar.uz/img/personal/"
